57c8e425fcaff59dce8b82a97eba0f316a782aa6
ArnaudDalcq <aranaud.dalcq@gmail.com>
2022-09-15 16:53:21 +0200

    Clean up project - Add Navbar Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/About" element={<About/>}/>
+          <Route path="/Portfolio" element={<Portfolio/>}/>
+          <Route path="/Contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 

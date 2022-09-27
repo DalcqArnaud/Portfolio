193400a5a1f3d40b2c1457846ce2bcec8858adf3
ArnaudDalcq <aranaud.dalcq@gmail.com>
2022-09-27 09:34:07 +0200

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './Components/Navbar/Navbar';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 import Home from './pages/Home/Home';
